a866e36b682178049e803b8cfcb7396e203ba021
lknell-cpu <lknell@salesforce.com>
2025-12-03 17:43:11 -0500

    Remove all authentication code and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AuthProvider } from './AuthContext.jsx'
-import ProtectedApp from './ProtectedApp.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <ProtectedApp />
-    </AuthProvider>
+    <App />
   </StrictMode>,
 )
